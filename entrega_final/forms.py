861115dfcb9721768886a2bc98553c89a1c08b82
dsalcedor <97924969+dsalcedor@users.noreply.github.com>
2022-06-07 00:43:00 -0500

    actualizacion
    se queda una pagina principal con opciones para ir a cualquiera de los 3 formularios

--- entrega_final/forms.py
+++ entrega_final/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 
 class ClienteForm(forms.Form):
-    razon_social = forms.CharField(label="Nombre", max_length=100)
+    razon_social = forms.CharField(label="Razon social", max_length=100)
     id_cliente = forms.CharField(label="Id_cliente", max_length=100)
     #widget=forms.TextInput(attrs={'placeholder': '1234'})
     #widget es para poder agregar un tip para que el usuario sepa como ingresar el valor
@@ -11,5 +11,5 @@ class ContactoForm(forms.Form):
     email = forms.EmailField()
 
 class EquipoForm(forms.Form):
-    equipo = forms.CharField(label="Nombre", max_length=100)
-    serie = forms.CharField(label="Nombre", max_length=100)
+    equipo = forms.CharField(label="Equipo", max_length=100)
+    serie = forms.CharField(label="Serie", max_length=100)
--- entrega_final/views.py
+++ entrega_final/views.py
@@ -1,8 +1,8 @@
 
 from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseRedirect
 from django.shortcuts import render
-from entrega_final.models import Cliente
-from entrega_final.forms import ClienteForm
+from entrega_final.models import Cliente, Contacto, Equipo
+from entrega_final.forms import ClienteForm, ContactoForm, EquipoForm
 from django.template import loader
 
 # Create your views here.
@@ -39,22 +39,62 @@ def borrar(request, identificador):
         return HttpResponseBadRequest("Error no conzco ese metodo para esta request")
 
 
-def actualizar(request, identificador):
-    pass
-
-
 
 def cliente(request):
-    return render(request, 'plantillas_final/cliente.html')
+    if request.method == "POST":
+        form = ClienteForm(request.POST)
+        if form.is_valid():
+
+            razon_social = form.cleaned_data['razon_social']
+            id_cliente = form.cleaned_data['id_cliente']
+            Cliente(razon_social=razon_social, id_cliente=id_cliente).save()
+
+            return HttpResponseRedirect('/entrega_final/')
+    elif request.method == "GET":
+        form = ClienteForm()
+    else:
+        return HttpResponseBadRequest("Error no conozco metodo")
+
+    return render(request, 'plantillas_final/cliente.html', {'form':form})
 
 def contacto(request):
-    return render(request, 'plantillas_final/contacto.html')
+    if request.method == "POST":
+        form = ContactoForm(request.POST)
+        if form.is_valid():
+
+            nombre = form.cleaned_data['razon_social']
+            email = form.cleaned_data['id_cliente']
+            Contacto(nombre=nombre, email=email).save()
+
+            return HttpResponseRedirect('/entrega_final/')
+    elif request.method == "GET":
+        form = ContactoForm()
+    else:
+        return HttpResponseBadRequest("Error no conozco metodo")
+
+    return render(request, 'plantillas_final/contacto.html', {'form':form})
 
 def equipo(request):
-    return render(request, 'plantillas_final/equipo.html')
+    if request.method == "POST":
+        form = EquipoForm(request.POST)
+        if form.is_valid():
+
+            equipo = form.cleaned_data['equipo']
+            serie = form.cleaned_data['serie']
+            Equipo(equipo=equipo, serie=serie).save()
+
+            return HttpResponseRedirect('/entrega_final/')
+    elif request.method == "GET":
+        form = EquipoForm()
+    else:
+        return HttpResponseBadRequest("Error no conozco metodo")
+
+    return render(request, 'plantillas_final/equipo.html', {'form':form})
 
 def formulario(request):
     return render(request,'plantillas_final/formulario.html')
 
+def actualizar(request, identificador):
+    pass
 
 
